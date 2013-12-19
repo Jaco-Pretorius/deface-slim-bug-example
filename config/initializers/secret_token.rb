a7e46cc70bf9a832c7487691b49647602bbe2b7f
Jaco Pretorius <jdjpretorius@gmail.com>
2013-12-19 15:55:04 -0500

    Created application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DefaceBug::Application.config.secret_token = '6cf352b70413f4be333500b7064bf7d167487df39ed9988ad2eaa3b0f487fc9e3b299a63b6c00ad6d95293829e4a74560b4617c7db9fca3fd824d21a90e6830a'
